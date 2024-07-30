e78aa195ff890a36856bf6458f9fa6c667c9ccc5
its-mahi <mahipalsuchar242@gmail.com>
2024-07-30 22:39:10 +0530

    [ Update ] : ignore numbers with value greater than 1000

--- src/StringCalculator.cpp
+++ src/StringCalculator.cpp
@@ -1,11 +1,10 @@
 #include "StringCalculator.h"
 #include <string>
 #include <stdexcept>
-#include <iostream>
-
 
 using namespace std;
 
+//  [ add ] : uses various methods and returns the sum of all positive numbers.
 int StringCalculator::add(const string &numbers) {
     
     if(numbers == "") {
@@ -21,6 +20,8 @@ int StringCalculator::add(const string &numbers) {
     return sum;
 }
 
+// [ parseNumber ] : parse the given string and return the vector of all positive numbers.
+
 vector<int> StringCalculator::parseNumbers(const string &numbers) {
 
     string processedNumbers = numbers;
@@ -29,6 +30,10 @@ vector<int> StringCalculator::parseNumbers(const string &numbers) {
     return splitNumbers(processedNumbers, delimiter);
 }
 
+/* 
+    [ extractDelimiter ] : extract delimiter from given string and return the value of delimiter, 
+                           if custom delimiter is not given then by default it returns ",\n"
+*/
 string StringCalculator::extractDelimiter(string &numbers) {
 
     if (numbers.substr(0, 2) == "//") {
@@ -46,6 +51,11 @@ string StringCalculator::extractDelimiter(string &numbers) {
 
     return ",\n";       // default delimiter
 }
+
+/* 
+    [ splitNumbers ] : returns vector of all positive numbers which is less than or equal to 1000
+                       seperate all numbers with given delimiter from the given string
+*/
 
 vector<int> StringCalculator::splitNumbers(const string &numbers, const string &delimiter) {
     vector<int> result;
@@ -72,19 +82,23 @@ vector<int> StringCalculator::splitNumbers(const string &numbers, const string &
         
         string numberString = numbers.substr(pos, delimiterPosition - pos);
         
+
+        // fetched next number and convert it into integer and push it into the vector
         if (numberString != "") {
             int num = stoi(numberString);
 
             if (num < 0) {
                 negativeNumbers.push_back(num);
             }
-            
-            result.push_back(num);
+            else if (num <= 1000) {
+                result.push_back(num);
+            }
         }
         
         pos = delimiterPosition + 1;
     }
 
+    // if there is any negative numbers present, throw an exception with message of all negative numbers mentioned
     if (!negativeNumbers.empty()) {
         string errorMessage = "Negative numbers not allowed : ";
 
--- test/StringCalculatorTest.cpp
+++ test/StringCalculatorTest.cpp
@@ -2,31 +2,53 @@
 #include "StringCalculator.h"
 using namespace std;
 
+
+/*
+    [ Test Case 1 ] : for empty string, result will be 0
+*/
 TEST(StringCalculatorTest, EmptyStringReturnsZero) {
     StringCalculator calc;
     ASSERT_EQ(calc.add(""), 0);
 }
 
+/*
+    [ Test Case 2 ] : single number containing string will give number itself
+*/
 TEST(StringCalculatorTest, SingleNumberReturnsItself) {
     StringCalculator calc;
     EXPECT_EQ(calc.add("1"), 1);
+    EXPECT_EQ(calc.add("24"), 24);
 }
 
+/*
+    [ Test Case 3 ] : two number seperated by comman (,) will return sum of both number
+*/
 TEST(StringCalculatorTest, TwoNumbersReturnSum) {
     StringCalculator calc;
     EXPECT_EQ(calc.add("1,5"), 6);
 }
 
+/*
+    [ Test Case 4 ] : multiple numbers seperated by comma (,) will return sum of all numbers
+*/
 TEST(StringCalculatorTest, HandlesMultipleNumbersReturnSum) {
     StringCalculator calc;
     EXPECT_EQ(calc.add("1,2,3,4,5"), 15);
 }
 
+/*
+    [ Test Case 5 ] : string with "\n", add method will seperate numbers by "\n" (consider similar to delimiter)
+*/
 TEST(StringCalculatorTest, HandlesNewLineDelimiter) {
     StringCalculator calc;
     EXPECT_EQ(calc.add("1\n2,3"), 6);
 }
 
+/*
+    [ Test Case 6 ] : string with custom delimiter and add method will seperate numbers by mentioned delimiter
+
+    NOTE : delimiter can be of any length, it will be handled
+*/
 TEST(StringCalculatorTest, HandlesCustomDelimiter) {
     StringCalculator calc;
 
@@ -37,8 +59,13 @@ TEST(StringCalculatorTest, HandlesCustomDelimiter) {
     EXPECT_EQ(calc.add("//:\n12\n2:1"), 15);
     EXPECT_EQ(calc.add("//:\n12\n2\n1"), 15);
     EXPECT_EQ(calc.add("//:::\n12:::2\n1"), 15);
+
+    EXPECT_EQ(calc.add("//:::\n122:::212\n112"), 446);
 }
 
+/*
+    [ Test Case 7 ] : string with negative numbers, add method will throw an exception will message of all negative numbers
+*/
 TEST(StringCalculatorTest, ThrowsExceptionForNegativeNumber) {
     StringCalculator calc;
 
@@ -66,4 +93,27 @@ TEST(StringCalculatorTest, ThrowsExceptionForNegativeNumber) {
     catch (...) {
         FAIL() << "Expected runtime_error";
     }
+
+    /*** 3 ***/
+    try {
+        calc.add("\n\n1,2,-3");
+        FAIL() << "Expected runtime_error";
+    } 
+    catch (const runtime_error &err) {
+        EXPECT_EQ(err.what(), string("Negative numbers not allowed : -3"));
+    } 
+    catch (...) {
+        FAIL() << "Expected runtime_error";
+    }
+}
+
+
+/*
+    [ Test Case 8 ] : string with number with value of greater than 1000, add method will ignore it
+*/
+TEST(StringCalculatorTest, NumbersGreaterThan1000) {
+    StringCalculator calc;
+    EXPECT_EQ(calc.add("2,1001"), 2);
+    EXPECT_EQ(calc.add("2,1000,5555,1001"), 1002);
+    EXPECT_EQ(calc.add("1,999,1001,1002"), 1000);
 }
